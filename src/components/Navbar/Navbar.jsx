7304a626643352e8f33dac558926387fe8408f3b
victor <victor_14x_02s@hotmail.com>
2023-12-06 18:16:59 -0300

    agregando codigo

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import classes from "./Navbar.modules.css"
+
+const Navbar = () => {
+  return (
+    <nav>
+      <h1 className={classes.rojo}>Ecommerce</h1>
+      <section>
+        <button>bebidas</button>
+        <button>galletas</button>
+        <button>dulces</button>
+      </section>
+    </nav>
+  )
+}
+
+export default Navbar
