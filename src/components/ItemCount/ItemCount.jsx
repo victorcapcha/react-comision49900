a9d459b4927fa360bb0889b91a49870a197d63b6
victor <victor_14x_02s@hotmail.com>
2023-12-07 17:10:34 -0300

    agregando modulos

---
+++ src/components/ItemCount/ItemCount.jsx
@@ -0,0 +1,11 @@
+const ItemCount = () => {
+  return (
+    <div>
+      <h1>0</h1>
+      <button>Decrementar</button>
+      <button>Incrementar</button>
+    </div>
+  )
+}
+
+export default ItemCount
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,12 +2,12 @@ import classes from "./Navbar.module.css"
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className={classes.container} style={{ display: "flex" , justifyContent: "space-between"}}>
       <h1 className={classes.rojo}>Ecommerce</h1>
-      <section>
-        <button>bebidas</button>
-        <button>galletas</button>
-        <button>dulces</button>
+      <section className={classes.categories}>
+        <button className="btn btn-cuccess">bebidas</button>
+        <button className="btn btn-danger">galletas</button>
+        <button className="btn btn-warning">dulces</button>
       </section>
     </nav>
   )
