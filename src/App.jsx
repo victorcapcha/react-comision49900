ef83b839ec442ac3175785519a7809cf13da6f0b
victor <victor_14x_02s@hotmail.com>
2023-12-12 17:43:05 -0300

    agregando codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Navbar from './components/Navbar/Navbar'
+import ItemCount from "./components/ItemCount/ItemCount"
+
 
 function App() {
   return (
     <>
-        <Navbar />
-      <h1>Comision 49900 Coderhouse</h1>
+       <Navbar />
+        <h1>Comision 49900 Coderhouse</h1>
+       <ItemCount initialValue={10}/>
+       <ItemCount initialValue={20}/>
     </>
   )
 }
--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react"
+
 const ItemCount = () => {
+  const [count, setCount] = useState()
+  //console.log(count)
+  const decrement = () => {
+    if(count > 0) {
+        setCount(count - 1)
+    }
+  }
+
+  const increment = () => {
+    setCount(count + 1)
+  }
+
   return (
     <div>
-      <h1>0</h1>
-      <button>Decrementar</button>
-      <button>Incrementar</button>
+      <h1>{count}</h1>
+      <button onClick={decrement}>Decrementar</button>
+      <button onClick={() => setCount(0)}>Reiniciar</button>
+      <button onClick={increment}>Incrementar</button>
     </div>
   )
 }
